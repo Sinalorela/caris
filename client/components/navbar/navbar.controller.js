6721d5ef2f6a20641f3c7ccfc7d7a15775112038
Sonia <sonia.amarandei@yahoo.com>
2014-07-29 14:10:11 +0300

    Am stilizat pagina principala: header, nav bar, search-skill, search by name;

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,15 +1,35 @@
 'use strict';
 
 angular.module('qweApp')
-  .controller('NavbarCtrl', function ($scope, $location) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }];
+    .controller('NavbarCtrl', function ($scope, $location) {
+        $scope.menu = [{
+            'title': 'Home',
+            'link': '/'
 
-    $scope.isCollapsed = true;
+        }];
+        $scope.import=[{
+            'title': 'import',
+            'link':'/add'
+        }];
 
-    $scope.isActive = function(route) {
-      return route === $location.path();
-    };
-  });
+
+        $scope.isCollapsed = true;
+        $scope.isActive = function(route) {
+            return route === $location.path();
+        };
+//        $scope.isCollapsed = true;
+//        $scope.isLoggedIn = Auth.isLoggedIn;
+//        $scope.isAdmin = Auth.isAdmin;
+//        $scope.getCurrentUser = Auth.getCurrentUser;
+
+        $scope.logout = function () {
+            Auth.logout();
+            $location.path('/login');
+        };
+
+        $scope.isActive = function (route) {
+            return route === $location.path();
+        };
+
+
+    });
